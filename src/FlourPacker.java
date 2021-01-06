6e20626e946f7ac0aa24c13ab53349db7b3b224e
SchnitzelB <es971120@icloud.com>
2021-01-05 22:40:47 -0600

    Tested new, consolidated conditional.

--- src/FlourPacker.java
+++ src/FlourPacker.java
@@ -2,7 +2,12 @@ public class FlourPacker {
 
     public static boolean canPack(int bigCount, int smallCount, int goal) {
 
+        int largeCount = bigCount * 5;
+        if ((bigCount < 0 ) || (smallCount < 0) || (goal < 0) || (largeCount + smallCount < goal)) {
+            return false;
+        }
 
+        return (smallCount >= goal % 5);
 
     }
 
