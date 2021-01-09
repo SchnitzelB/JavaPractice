e5ea2829eca1379b90eb40e97e41575cd5ae7f18
SchnitzelB <es971120@icloud.com>
2021-01-08 23:09:16 -0600

    Committed ReadingUserInputChallenge.java.

---
+++ src/ReadingUserInputChallenge.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ReadingUserInputChallenge {
+
+    public static void main(String []args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter number #x: ");
+
+    }
+
+}
