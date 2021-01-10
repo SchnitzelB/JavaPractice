75ecf941f32c3f17ffa03ac2eba502a6e9b2a2db
SchnitzelB <es971120@icloud.com>
2021-01-09 21:31:10 -0600

    Removed break from else statement to prevent from terminating the code when hasNextInt() method is false. Instead, it gives the user a chance to input an int.

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main {
         int sum = 0;
         int i = 1;
 
-        while (i <= 10) {
+        while (true) {
             System.out.println("Enter number #" + i + ": ");
 
             boolean isAnInt = scanner.hasNextInt();
@@ -22,9 +22,11 @@ public class Main {
                 int number = scanner.nextInt();
                 sum += number;
                 i++;
+                if (i == 10) {
+                    break;
+                }
             } else {
                 System.out.println("Invalid Number");
-                break;
             }
 
             scanner.nextLine(); // handle end of line (enter key)
