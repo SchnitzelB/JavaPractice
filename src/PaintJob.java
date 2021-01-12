d97ef8dd62105b581a50366cceed5cc949da794b
SchnitzelB <es971120@icloud.com>
2021-01-11 21:51:11 -0600

    Added two overloaded methods for specific scenarios.

--- src/PaintJob.java
+++ src/PaintJob.java
@@ -1,5 +1,33 @@
 public class PaintJob {
 
+    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets) {
 
+        if ((width <= 0) || (height <= 0) || (areaPerBucket <= 0) || (extraBuckets < 0)) {
+            return -1;
+        } else {
+            double bucketCount = Math.ceil((width * height) / areaPerBucket);
+            bucketCount -= extraBuckets;
+            return (int) bucketCount;
+        }
+    }
 
+    public static int getBucketCount(double width, double height, double areaPerBucket) {
+
+        if ((width <= 0) || (height <= 0) || (areaPerBucket <= 0)) {
+            return -1;
+        } else {
+            return (int) Math.ceil(((width * height) / areaPerBucket));
+        }
+
+    }
+
+    public static int getBucketCount(double area, double areaPerBucket) {
+
+        if ((area <= 0) || (areaPerBucket <= 0)) {
+            return -1;
+        } else {
+            return (int) Math.ceil(area / areaPerBucket);
+        }
+
+    }
 }
