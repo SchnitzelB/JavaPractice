a415ee23da29360fbca178a0c653e2d0882ba553
SchnitzelB <es971120@icloud.com>
2021-01-12 20:51:54 -0600

    Committed SimpleCalculator.java. Utilizes instance variable and methods to return the sum of two doubles.

---
+++ src/SimpleCalculator.java
@@ -0,0 +1,21 @@
+public class SimpleCalculator {
+
+    private double firstNumber;
+    private double secondNumber;
+
+    public double getFirstNumber() {
+        return this.firstNumber;
+    }
+
+    public double getSecondNumber() {
+        return this.secondNumber;
+    }
+
+    public void setFirstNumber(double firstNumber) {
+        this.firstNumber = firstNumber;
+    }
+
+    public void setSecondNumber(double secondNumber) {
+        this.secondNumber = secondNumber;
+    }
+}
