36b48fc319d3cb5909d6cca689da828cddfc8556
SchnitzelB <es971120@icloud.com>
2021-01-20 12:09:49 -0600

    Initial commit: ComplexNumber.java.

---
+++ src/ComplexNumber.java
@@ -0,0 +1,22 @@
+public class ComplexNumber {
+
+    private double real, imaginary;
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public double getReal() {
+        return this.real;
+    }
+
+    public double getImaginary() {
+        return this.imaginary;
+    }
+
+    public double add(double real, double imaginary) {
+
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -47,7 +47,40 @@ public class Main {
 
         System.out.println("Point Challenge");
 
+        Point first = new Point(6,5);
+        Point second = new Point(3,1);
 
+        System.out.println("Distance(0,0) = " + first.distance());
+        System.out.println("Distance(second) = " + second.distance(second));
+        System.out.println("Distance(2,2) = " + first.distance(2,2));
+
+        Point point = new Point();
+
+        System.out.println("Distance() = " + point.distance());
+
+        System.out.println();
+        System.out.println("-------------------------");
+        System.out.println();
+
+        System.out.println("Complex Number Challenge");
+
+        ComplexNumber one = new ComplexNumber(1.0,1.0);
+        ComplexNumber number = new ComplexNumber(2.5,-1.5);
+
+        one.add(1,1);
+
+        System.out.println("one.real = " + one.getReal());
+        System.out.println("one.imaginary = " + one.getImaginary());
+
+        one.subtract(number);
+
+        System.out.println("one.real = " + one.getReal());
+        System.out.println("one.imaginary = " + one.getImaginary());
+
+        number.subtract(one);
+
+        System.out.println("number.real = " + number.getReal());
+        System.out.println("number.imaginary = " + number.getImaginary());
 
         System.out.println();
         System.out.println("-------------------------");
